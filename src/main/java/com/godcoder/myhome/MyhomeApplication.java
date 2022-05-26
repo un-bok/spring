4ec1daef5458a790da873815ca524b52b3ede1fa
un-bok <boham0510@gmail.com>
2022-05-26 19:26:53 +0900

    init2

--- src/main/java/com/godcoder/myhome/MyhomeApplication.java
+++ src/main/java/com/godcoder/myhome/MyhomeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyhomeApplication {
-
+asdas
 	public static void main(String[] args) {
 		SpringApplication.run(MyhomeApplication.class, args);
 	}
